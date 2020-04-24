29ba6f2b7586d2a33ef5ecfef71f3002d771bf41
bbanderson <48074659+bbanderson@users.noreply.github.com>
2020-04-24 23:34:25 +0900

    Update clock.js

--- clock/static/js/clock.js
+++ clock/static/js/clock.js
@@ -1,15 +1,42 @@
 const CLOCK = document.querySelector(".js-clock");
 
-function showClock() {
-    const DATE = new Date();
+function getTime() {
+    
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const now = new Date();
 
-    const HOUR = DATE.getHours();
-    const MIN = DATE.getMinutes();
-    const SEC = DATE.getSeconds();
+  // milliseconds raw data
+  const xmasMilli = xmasDay.getTime();
+  const nowMilli = now.getTime();
+  const timeMilliInterval = xmasMilli - nowMilli;
+  
+  const MILLI_TO_SEC = 1000;
+  const SECONDS_IN_MINUTES = 60;
+  const MINUTES_IN_HOUR = 60;
+  const HOURS_IN_DAY = 24;
 
-    CLOCK.innerText =
-     `${ HOUR < 10? `0${HOUR}` : HOUR} : ${ MIN < 10? `0${MIN}` : MIN} : ${ SEC < 10? `0${SEC}` : SEC}`;
+  const diffDays = Math.floor(timeMilliInterval / (MILLI_TO_SEC * SECONDS_IN_MINUTES * MINUTES_IN_HOUR * HOURS_IN_DAY)); 
+  const diffHours = Math.floor(timeMilliInterval / (MILLI_TO_SEC * SECONDS_IN_MINUTES * MINUTES_IN_HOUR) % HOURS_IN_DAY);
+  const diffMinutes = Math.floor(timeMilliInterval / (MILLI_TO_SEC * SECONDS_IN_MINUTES) % MINUTES_IN_HOUR);
+  const diffSeconds = Math.floor(timeMilliInterval / (MILLI_TO_SEC) % SECONDS_IN_MINUTES);
+
+  console.log(diffDays, diffHours, diffMinutes, diffSeconds);
+
+  let FORMAT =
+    `${diffDays < 10 ? `0${diffDays}` : `${diffDays}`}d ` +
+    `${diffHours < 10 ? `0${diffHours}` : `${diffHours}`}h ` +
+    `${diffMinutes < 10 ? `0${diffMinutes}` : `${diffMinutes}`}m ` +
+    `${diffSeconds < 10 ? `0${diffSeconds}` : `${diffSeconds}`}s`;
+
+  TIME_VIEW.innerText = FORMAT;
+
 }
+
+function init() {
+  setInterval(getTime, 1000)
+}
+init();
+
 
 function init() {
     setInterval(showClock, 1000);
